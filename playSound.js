3bf983c60bcbbeb611b5a1b4cb41ff3d0d8048b0
benadaephondelat <georgi_iliev@yahoo.com>
2014-10-14 14:22:44 +0300

    Drop-down Menu, Nakov page
    added the drop-down menu's first version, and a Nakov html page, need to
    fix some bugs with the audio

---
+++ playSound.js
@@ -0,0 +1,13 @@
+function play() {
+    var helloSound = document.getElementById("sound");
+    helloSound.pause();
+    helloSound.load();
+    helloSound.play();
+}
+
+function playAngry() {
+    var helloSound = document.getElementById("angry");
+    helloSound.pause();
+    helloSound.load();
+    helloSound.play();
+}
