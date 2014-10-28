9f6b65be3987dd1d2c149d6200a1f8395ea057dc
benadaephondelat <georgi_iliev@yahoo.com>
2014-10-28 11:20:34 +0200

    another gag plus file organization

---
+++ scripts/playSound.js
@@ -0,0 +1,282 @@
+function play() {
+    var helloSound = document.getElementById("sound");
+    helloSound.pause();
+    helloSound.load();
+    helloSound.play();
+}
+
+function stopAzis() {
+    var thissound=document.getElementById("sound");
+    thissound.pause();
+    thissound.currentTime = 0;
+}
+
+function playAngry() {
+    var helloSound = document.getElementById("angry");
+    helloSound.pause();
+    helloSound.load();
+    helloSound.play();
+}
+
+function stopAngry() {
+    var helloSound = document.getElementById("angry");
+    helloSound.pause();
+    helloSound.currentTime = 0;
+
+}
+
+function playBorat() {
+    var helloSound = document.getElementById("borat");
+    helloSound.pause();
+    helloSound.load();
+    helloSound.play();
+}
+
+function stopBorat() {
+    var helloSound = document.getElementById("borat");
+    helloSound.pause();
+    helloSound.currentTime = 0;
+}
+
+function playClothes() {
+    var helloSound = document.getElementById("clothes");
+    helloSound.pause();
+    helloSound.load();
+    helloSound.play();
+}
+
+function stopClothes() {
+    var helloSound = document.getElementById("clothes");
+    helloSound.pause();
+    helloSound.currentTime = 0;
+}
+
+
+function playWest() {
+    var helloSound = document.getElementById("west");
+    helloSound.pause();
+    helloSound.load();
+    helloSound.play();
+}
+
+function stopWest() {
+    var helloSound = document.getElementById("west");
+    helloSound.pause();
+    helloSound.currentTime = 0;
+}
+
+function playGladiator() {
+    var helloSound = document.getElementById("gladiator");
+    helloSound.pause();
+    helloSound.load();
+    helloSound.play();
+}
+
+function stopGladiator() {
+    var helloSound = document.getElementById("gladiator");
+    helloSound.pause();
+    helloSound.currentTime = 0;
+}
+
+function playFuck() {
+    var helloSound = document.getElementById("fuck");
+    helloSound.pause();
+    helloSound.load();
+    helloSound.play();
+}
+
+function stopFuck() {
+    var helloSound = document.getElementById("fuck");
+    helloSound.pause();
+    helloSound.currentTime = 0;
+}
+
+function playPR() {
+    var helloSound = document.getElementById("time");
+    helloSound.pause();
+    helloSound.load();
+    helloSound.play();
+}
+
+function stopPR() {
+    var helloSound = document.getElementById("time");
+    helloSound.pause();
+    helloSound.currentTime = 0;
+}
+
+function playBond() {
+    var helloSound = document.getElementById("bond");
+    helloSound.pause();
+    helloSound.load();
+    helloSound.play();
+}
+
+function stopBond() {
+    var helloSound = document.getElementById("bond");
+    helloSound.pause();
+    helloSound.currentTime = 0;
+}
+
+function playHello() {
+    var helloSound = document.getElementById("hello");
+    helloSound.pause();
+    helloSound.load();
+    helloSound.play();
+}
+
+function stopHello() {
+    var helloSound = document.getElementById("hello");
+    helloSound.pause();
+    helloSound.currentTime = 0;
+}
+
+
+function playFire() {
+    var helloSound = document.getElementById("fire");
+    helloSound.pause();
+    helloSound.load();
+    helloSound.play();
+}
+
+function stopFire() {
+    var helloSound = document.getElementById("fire");
+    helloSound.pause();
+    helloSound.currentTime = 0;
+}
+
+function playRambo() {
+    var helloSound = document.getElementById("rambo");
+    helloSound.pause();
+    helloSound.load();
+    helloSound.play();
+}
+
+function stopRambo() {
+    var helloSound = document.getElementById("rambo");
+    helloSound.pause();
+    helloSound.currentTime = 0;
+}
+
+function playLaugh() {
+    var helloSound = document.getElementById("laugh");
+    helloSound.pause();
+    helloSound.load();
+    helloSound.play();
+}
+
+function stopLaugh() {
+    var helloSound = document.getElementById("laugh");
+    helloSound.pause();
+    helloSound.currentTime = 0;
+}
+
+function playSuper() {
+    var helloSound = document.getElementById("super");
+    helloSound.pause();
+    helloSound.load();
+    helloSound.play();
+}
+
+function stopSuper() {
+    var helloSound = document.getElementById("super");
+    helloSound.pause();
+    helloSound.currentTime = 0;
+}
+
+
+function playMoney() {
+    var helloSound = document.getElementById("money");
+    helloSound.pause();
+    helloSound.load();
+    helloSound.play();
+}
+
+function stopMoney() {
+    var helloSound = document.getElementById("money");
+    helloSound.pause();
+    helloSound.currentTime = 0;
+}
+
+function playScores() {
+    var helloSound = document.getElementById("scores");
+    helloSound.pause();
+    helloSound.load();
+    helloSound.play();
+}
+
+function stopScores() {
+    var helloSound = document.getElementById("scores");
+    helloSound.pause();
+    helloSound.currentTime = 0;
+
+}
+
+function playMachine() {
+    var helloSound = document.getElementById("machine");
+    helloSound.pause();
+    helloSound.load();
+    helloSound.play();
+}
+
+function stopMachine() {
+    var helloSound = document.getElementById("machine");
+    helloSound.pause();
+    helloSound.currentTime = 0;
+
+}
+
+function playWolverine() {
+    var helloSound = document.getElementById("wolverine");
+    helloSound.pause();
+    helloSound.load();
+    helloSound.play();
+}
+
+function stopWolverine() {
+    var helloSound = document.getElementById("wolverine");
+    helloSound.pause();
+    helloSound.currentTime = 0;
+
+}
+function playJungle() {
+    var helloSound = document.getElementById("jungle");
+    helloSound.pause();
+    helloSound.load();
+    helloSound.play();
+}
+
+function stopJungle() {
+    var helloSound = document.getElementById("jungle");
+    helloSound.pause();
+    helloSound.currentTime = 0;
+
+}
+
+function playKombat() {
+    var helloSound = document.getElementById("kombat");
+    helloSound.pause();
+    helloSound.load();
+    helloSound.play();
+}
+
+function stopKombat() {
+    var helloSound = document.getElementById("kombat");
+    helloSound.pause();
+    helloSound.currentTime = 0;
+
+}
+
+function playPassword() {
+    var helloSound = document.getElementById("password");
+    helloSound.pause();
+    helloSound.load();
+    helloSound.play();
+}
+
+function stopPassword() {
+    var helloSound = document.getElementById("password");
+    helloSound.pause();
+    helloSound.currentTime = 0;
+
+}
+
